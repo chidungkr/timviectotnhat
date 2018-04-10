513b85ecd1b510bc2835c0d0350263c79daeb981
Nguyen Chi Dung <nguyenchidung.neu@gmail.com>
2018-04-10 15:57:01 +0700

    Add files via upload

--- crawler_viectotnhat.R
+++ crawler_viectotnhat.R
@@ -14,6 +14,7 @@ rm(list = ls())
 
 
 get_link_for_job_group <- function(x) {
+  Sys.sleep(5)
   m <- read_lines(x)
   m <- m[str_detect(m, "https://")]
   m <- m[str_detect(m, "title=")]
@@ -47,12 +48,16 @@ job_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-kinh-do
 #  Hàm lấy các thông tin về công việc được đăng tuyển
 #-----------------------------------------------------
 
+x <- "https://viectotnhat.com/tuyen-nhan-vien-kinh-doanh-ha-noi-1112855.html"
+
 
 get_information_for_job <- function(x) {
   
+  Sys.sleep(5)
   k <- http_status(GET(x))
   
   if (k$category == "Success") {
+    
     m <- read_html(x)
     
     wage <- m %>%
@@ -118,6 +123,33 @@ get_information_for_job <- function(x) {
     
     if (str_count(gioi_tinh) == 0) {gioi_tinh <- NA}
     
+    job_name <- m %>%
+      html_nodes(xpath = '//*[@id="main-content"]/div/div/div[1]/div[2]/div[1]/h1') %>% 
+      html_text() %>% 
+      str_trim()
+    
+    if (str_count(job_name) == 0) {job_name <- NA}
+    
+    com_name <- m %>%
+      html_nodes(xpath = '//*[@id="main-content"]/div/div/div[1]/div[2]/div[1]/h2') %>% 
+      html_text() %>% 
+      str_trim()
+    
+    if (str_count(com_name) == 0) {com_name <- NA}
+    
+    com_add <- m %>%
+      html_nodes(xpath = '//*[@id="main-content"]/div/div/div[1]/div[4]/div/div[1]/div[1]/div[9]/ul/li[2]') %>% 
+      html_text() %>% 
+      str_trim()
+    
+    if (str_count(com_add) == 0) {com_add <- NA}
+    
+    deadline <- m %>%
+      html_nodes(xpath = '//*[@id="main-content"]/div/div/div[1]/div[2]/div[1]/div[2]') %>% 
+      html_text() %>% 
+      str_trim()
+    
+    if (str_count(deadline) == 0) {deadline <- NA}
     
     all_info <- data.frame(luong = wage, 
                            kinh_nghiem = kinh_nghiem, 
@@ -128,6 +160,10 @@ get_information_for_job <- function(x) {
                            position = position, 
                            hinh_thuc = hinh_thuc, 
                            gioi_tinh = gioi_tinh, 
+                           job_name = job_name, 
+                           com_name = com_name, 
+                           com_add = com_add, 
+                           deadline = deadline, 
                            link_source = x)
     
   }
@@ -136,17 +172,98 @@ get_information_for_job <- function(x) {
   
 }
 
+# Test hàm: 
+
 job_link$job_link[2] %>% get_information_for_job()
 
-# Hàm lấy các thông tin cho một list các link về công việc: 
+# Hàm lấy các thông tin cho một list các link về công việc phương án 1: 
 
-get_information_for_group_job <- function(x) {
+get_information_for_group_job1 <- function(x) {
   a <- lapply(x, get_information_for_job)
   return(do.call("rbind", a))
 }
 
+get_information_for_group_job1(job_link$job_link[1:2]) # Kiểu này có thể lỗi connection. 
 
-get_information_for_group_job(job_link$job_link)
+# Phương án 2: 
+
+get_information_for_group_job2 <- function(list_job_link) {
+  n <- length(list_job_link)
+  job_inf <- vector("list", length = n)
+  for (i in seq_along(list_job_link)) {
+    Sys.sleep(3)
+    job_inf[[i]] <- get_information_for_job(list_job_link[i])
+  }
+  job_inf <- do.call("bind_rows", job_inf)
+  return(job_inf)
+}
+
+get_information_for_group_job2(job_link$job_link[1:2])
+
+
+# Phương án 3:  
+
+get_information_for_group_job3 <- function(list_job_link) {
+  job_inf <- tryCatch(lapply(list_job_link, get_information_for_job))
+  job_inf <- do.call("bind_rows", job_inf)
+  return(job_inf)
+}
+
+get_information_for_group_job3(job_link$job_link[1:2])
+
+
+# Phương án 4 (tham khảo: https://somerealnumbers.wordpress.com/2015/05/17/11/): 
+
+
+get_information_for_group_job4 <- function(list_job_link) {
+  n <- length(list_job_link)
+  all_df <- data.frame()
+  for (i in 1:n) {
+    job_inf <- tryCatch({get_information_for_job(list_job_link[i])}, 
+                        error = function(i){return(data.frame())})
+    all_df <- bind_rows(all_df, job_inf)
+    
+  }
+  return(all_df)
+}
+
+get_information_for_group_job4(job_link$job_link[1:2])
+
+
+
+# Test tốc độ: 
+library(microbenchmark)
+
+microbenchmark(
+  u1 <- get_information_for_group_job1(job_link$job_link[1:2]), 
+  u2 <- get_information_for_group_job2(job_link$job_link[1:2]), 
+  u3 <- get_information_for_group_job3(job_link$job_link[1:2]), 
+  u4 <- get_information_for_group_job4(job_link$job_link[1:2]),
+  times = 3
+  
+)
+
+# Phương án 2 lâu nhất với thời gian trung bình là 17 giây, Phương án 1, 3
+# và 4 có thời gian trung bình thấp hơn cả. 
+
+
+#-----------------------------------------------
+#  Tất cả việc thuộc nhóm kinh doanh  (1100)
+#-----------------------------------------------
+
+kinh_doanh_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-kinh-doanh-f32.html?page=", 60)
+kinh_doanh_job <- get_information_for_group_job4(kinh_doanh_link$job_link)
+
+
+# n <- length(kinh_doanh_link$job_link)
+# 
+# all_df_kinh_doanh <- data.frame()
+# for (i in 1:n) {
+#   job_inf <- tryCatch({get_information_for_job(kinh_doanh_link$job_link[i])}, 
+#                       error = function(i) {return(data.frame())})
+#   all_df_kinh_doanh <- bind_rows(all_df_kinh_doanh, job_inf)
+#   }
+
 
 
 
