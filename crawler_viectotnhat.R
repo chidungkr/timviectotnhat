b08abae2fb235e62a504efbeeeb6ab79d260b58b
Nguyen Chi Dung <nguyenchidung.neu@gmail.com>
2018-04-17 08:32:46 +0700

    Add files via upload

--- crawler_viectotnhat.R
+++ crawler_viectotnhat.R
@@ -1,12 +1,16 @@
 
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#     Phần 1: Thu thập dữ liệu từ web và xử lí sơ bộ
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
 
 library(tidyverse)
 library(magrittr)
 library(rvest)
 library(stringr)
 library(httr)
-library(httr)
-rm(list = ls())
+
+
 
 #----------------------------------------------------------------
 #  Hàm lấy link của một nhóm CV chọn trước được đăng trên 1 page
@@ -38,17 +42,18 @@ get_all_link_for_job_group <- function(base_url, n_pages) {
   all_link <- all_link[!duplicated(all_link)] %>% as.character()
   all_link <- data.frame(job_link = all_link, 
                          job_type = c(rep(str_sub(base_url, 34, str_count(base_url) - 15))))
+  all_link <- all_link %>% mutate(job_group = base_url)
   return(all_link %>% mutate_if(is.factor, as.character))
 }
 
-job_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-kinh-doanh-f32.html?page=", 1)
+# job_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-kinh-doanh-f32.html?page=", 1)
 
 
 #-----------------------------------------------------
 #  Hàm lấy các thông tin về công việc được đăng tuyển
 #-----------------------------------------------------
 
-x <- "https://viectotnhat.com/tuyen-nhan-vien-kinh-doanh-ha-noi-1112855.html"
+# x <- "https://viectotnhat.com/tuyen-nhan-vien-kinh-doanh-ha-noi-1112855.html"
 
 
 get_information_for_job <- function(x) {
@@ -174,7 +179,7 @@ get_information_for_job <- function(x) {
 
 # Test hàm: 
 
-job_link$job_link[2] %>% get_information_for_job()
+# job_link$job_link[2] %>% get_information_for_job()
 
 # Hàm lấy các thông tin cho một list các link về công việc phương án 1: 
 
@@ -198,7 +203,7 @@ get_information_for_group_job2 <- function(list_job_link) {
   return(job_inf)
 }
 
-get_information_for_group_job2(job_link$job_link[1:2])
+# get_information_for_group_job2(job_link$job_link[1:2])
 
 
 # Phương án 3:  
@@ -209,7 +214,7 @@ get_information_for_group_job3 <- function(list_job_link) {
   return(job_inf)
 }
 
-get_information_for_group_job3(job_link$job_link[1:2])
+# get_information_for_group_job3(job_link$job_link[1:2])
 
 
 # Phương án 4 (tham khảo: https://somerealnumbers.wordpress.com/2015/05/17/11/): 
@@ -227,17 +232,17 @@ get_information_for_group_job4 <- function(list_job_link) {
   return(all_df)
 }
 
-get_information_for_group_job4(job_link$job_link[1:2])
+# get_information_for_group_job4(job_link$job_link[1:2])
 
 
 
-# Test tốc độ: 
+# Test tốc độ:
 library(microbenchmark)
 
 microbenchmark(
-  u1 <- get_information_for_group_job1(job_link$job_link[1:2]), 
-  u2 <- get_information_for_group_job2(job_link$job_link[1:2]), 
-  u3 <- get_information_for_group_job3(job_link$job_link[1:2]), 
+  u1 <- get_information_for_group_job1(job_link$job_link[1:2]),
+  u2 <- get_information_for_group_job2(job_link$job_link[1:2]),
+  u3 <- get_information_for_group_job3(job_link$job_link[1:2]),
   u4 <- get_information_for_group_job4(job_link$job_link[1:2]),
   times = 3
   
@@ -247,118 +252,137 @@ microbenchmark(
 # và 4 có thời gian trung bình thấp hơn cả. 
 
 
+
+# Nhân sự: 
+
+nhan_su_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-nhan-su-f40.html?page=", 13)
+nhan_su_job <- get_information_for_group_job4(nhan_su_link$job_link[!duplicated(nhan_su_link$job_link)])
+
+# Thiết kế - mĩ thuật: 
+
+thiet_ke_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-thiet-ke-my-thuat-f49.html?page=", 12)
+thiet_ke_job <- get_information_for_group_job4(thiet_ke_link$job_link[!duplicated(thiet_ke_link$job_link)])
+
+# Xây dựng: 
+xay_dung_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-xay-dung-f52.html?page=", 15)
+xay_dung_job <- get_information_for_group_job4(xay_dung_link$job_link[!duplicated(xay_dung_link$job_link)])
+
 # Giáo dục - đào tạo: 
 
 giao_duc_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-giao-duc-dao-tao-thu-vien-f26.html?page=", 11)
-giao_duc_job <- get_information_for_group_job4(giao_duc_link$job_link)
+giao_duc_job <- get_information_for_group_job4(giao_duc_link$job_link[!duplicated(giao_duc_link$job_link)])
 
 # Kiến trúc - nội thất: 
 
 kien_truc_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-kien-truc-noi-that-f31.html?page=", 11)
-kien_truc_job <- get_information_for_group_job4(kien_truc_link$job_link)
+kien_truc_job <- get_information_for_group_job4(kien_truc_link$job_link[!duplicated(kien_truc_link$job_link)])
 
 
 # Nhóm công việc không tên:
 khac_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-khac-f55.html?page=", 9)
-khac_job <- get_information_for_group_job4(khac_link$job_link)
+khac_job <- get_information_for_group_job4(khac_link$job_link[!duplicated(khac_link$job_link)])
 
 
 # Bảo Hiểm: 
 
 bao_hiem_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-tu-van-bao-hiem-f11.html?page=", 8)
-bao_hiem_job <- get_information_for_group_job4(bao_hiem_link$job_link)
-
-# Nhân sự: 
-
-nhan_su_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-nhan-su-f40.html?page=", 13)
-nhan_su_job <- get_information_for_group_job4(nhan_su_link$job_link)
-
-# Thiết kế - mĩ thuật: 
-
-thiet_ke_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-thiet-ke-my-thuat-f49.html?page=", 12)
-thiet_ke_job <- get_information_for_group_job4(thiet_ke_link$job_link)
-
-# Xây dựng: 
-xay_dung_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-xay-dung-f52.html?page=", 15)
-xay_dung_job <- get_information_for_group_job4(xay_dung_link$job_link)
+bao_hiem_job <- get_information_for_group_job4(bao_hiem_link$job_link[!duplicated(bao_hiem_link$job_link)])
 
 
 # Hành chính - thư kí - trợ lí (500): 
 
 hanh_chinh_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-hanh-chinh-thu-ky-tro-ly-f29.html?page=", 33)
-hanh_chinh_job <- get_information_for_group_job4(hanh_chinh_link$job_link)
+hanh_chinh_job <- get_information_for_group_job4(hanh_chinh_link$job_link[!duplicated(hanh_chinh_link$job_link)])
 
 
 # Sinh viên ra trường - thực tập học việc: 
 
 sinh_vien_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-sinh-vien-moi-tot-nghiep-thuc-tap-f35.html?page=", 24)
-sinh_vien_job <- get_information_for_group_job4(sinh_vien_link$job_link)
+sinh_vien_job <- get_information_for_group_job4(sinh_vien_link$job_link[!duplicated(sinh_vien_link$job_link)])
 
 
 # Marketing (500): 
 
 marketing_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-quang-cao-marketing-pr-f45.html?page=", 34)
-marketing_job <- get_information_for_group_job4(marketing_link$job_link)
+marketing_job <- get_information_for_group_job4(marketing_link$job_link[!duplicated(marketing_link$job_link)])
 
 # Bất động sản: 
 bat_dong_san_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-bat-dong-san-f13.html?page=", 30)
-bat_dong_san_job <- get_information_for_group_job4(bat_dong_san_link$job_link)
+bat_dong_san_job <- get_information_for_group_job4(bat_dong_san_link$job_link[!duplicated(bat_dong_san_link$job_link)])
 
 # Công Nghệ Thông Tin: 
 cong_nghe_tt_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-cong-nghe-thong-tin-f17.html?page=", 23)
-cong_nghe_tt_job <- get_information_for_group_job4(cong_nghe_tt_link$job_link)
+cong_nghe_tt_job <- get_information_for_group_job4(cong_nghe_tt_link$job_link[!duplicated(cong_nghe_tt_link$job_link)])
 
 # Cơ khí: 
 
 co_khi_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-co-khi-ki-thuat-ung-dung-f16.html?page=", 21)
-co_khi_job <- get_information_for_group_job4(co_khi_link$job_link)
+co_khi_job <- get_information_for_group_job4(co_khi_link$job_link[!duplicated(co_khi_link$job_link)])
 
 # Du lịch - nhà hàng - khách sạn: 
 nha_hang_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-du-lich-nha-hang-khach-san-f23.html?page=", 30)
-nha_hang_job <- get_information_for_group_job4(nha_hang_link$job_link)
+nha_hang_job <- get_information_for_group_job4(nha_hang_link$job_link[!duplicated(nha_hang_link$job_link)])
 
 # Điện tử - điện lạnh: 
 dien_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-dien-dien-tu-dien-lanh-f22.html?page=", 30)
-dien_job <- get_information_for_group_job4(dien_link$job_link)
+dien_job <- get_information_for_group_job4(dien_link$job_link[!duplicated(dien_link$job_link)])
 
-
-# Chăm sóc khách hàng (650): 
+# Chăm sóc khách hàng (650) - làm ngày 16 - 04: 
 
 cham_soc_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-cham-soc-khach-hang-f21.html?page=", 43)
-ban_hang_job <- get_information_for_group_job4(cham_soc_link$job_link)
+cham_soc_job_16_04 <- get_information_for_group_job4(cham_soc_link$job_link[!duplicated(cham_soc_link$job_link)])
 
-# Việc bán hàng (1000): 
-ban_hang_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-ban-hang-f10.html?page=", 69)
-ban_hang_job <- get_information_for_group_job4(ban_hang_link$job_link)
 
 # Việc kinh doanh: 
 
-kinh_doanh_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-kinh-doanh-f32.html?page=", 75)
-kinh_doanh_job <- get_information_for_group_job4(kinh_doanh_link$job_link)
 
- 
+kinh_doanh_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-kinh-doanh-f32.html?page=", 75)
+kinh_doanh_job <- get_information_for_group_job4(kinh_doanh_link$job_link[!duplicated(kinh_doanh_link$job_link)])
+
+
+# Việc bán hàng (1000): 
+ban_hang_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-ban-hang-f10.html?page=", 69)
+ban_hang_job <- get_information_for_group_job4(ban_hang_link$job_link[!duplicated(ban_hang_link$job_link)])
+
+
 # Lao động phổ thông (450): 
 
 lao_dong_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-lao-dong-pho-thong-f33.html?page=", 300)  
-lao_dong_job <- get_information_for_group_job4(lao_dong_link$job_link)
+lao_dong_job <- get_information_for_group_job4(lao_dong_link$job_link[!duplicated(lao_dong_link$job_link)])
 
 
 # Tài Chính - Kế Toán: 
 
 tai_chinh_link <- get_all_link_for_job_group("https://viectotnhat.com/viec-lam-tai-chinh-ke-toan-kiem-toan-f47.html?page=", 100)
-tai_chinh_job <- get_information_for_group_job4(tai_chinh_link$job_link)
+tai_chinh_job <- get_information_for_group_job4(tai_chinh_link$job_link[!duplicated(tai_chinh_link$job_link)])
 
 
+# load("D:/job_anh_cuong/viec_lam.RData")
+
+library(tidyverse)
+library(magrittr)
+
+all_job_data <- bind_rows(ban_hang_job %>% mutate(job_group = "viec-lam-ban-hang"), 
+                          bao_hiem_job %>% mutate(job_group = "viec-lam-tu-van-bao-hiem"), 
+                          bat_dong_san_job %>% mutate(job_group = "viec-lam-bat-dong-san"), 
+                          cham_soc_job_16_04 %>% mutate(job_group = "viec-lam-cham-soc-khach-hang"), 
+                          co_khi_job %>% mutate(job_group = "viec-lam-co-khi-ki-thuat-ung-dung"), 
+                          cong_nghe_tt_job %>% mutate(job_group = "viec-lam-cong-nghe-thong-tin"), 
+                          dien_job %>% mutate(job_group = "viec-lam-dien-dien-tu-dien-lanh"), 
+                          giao_duc_job %>% mutate(job_group = "viec-lam-giao-duc-dao-tao-thu-vien"), 
+                          hanh_chinh_job %>% mutate(job_group = "viec-lam-hanh-chinh-thu-ky-tro-ly"), 
+                          khac_job %>% mutate(job_group = "viec-lam-khac"), 
+                          kien_truc_job %>% mutate(job_group = "viec-lam-kien-truc-noi-that"), 
+                          kinh_doanh_job2 %>% mutate(job_group = "viec-lam-kinh-doanh"), 
+                          lao_dong_job %>% mutate(job_group = "viec-lam-lao-dong-pho-thong"), 
+                          marketing_job %>% mutate(job_group = "viec-lam-quang-cao-marketing-pr"), 
+                          nha_hang_job %>% mutate(job_group = "viec-lam-du-lich-nha-hang-khach-san"), 
+                          nhan_su_job %>% mutate(job_group = "viec-lam-nhan-su"), 
+                          sinh_vien_job %>% mutate(job_group = "viec-lam-sinh-vien-moi-tot-nghiep-thuc-tap"), 
+                          tai_chinh_job %>% mutate(job_group = "viec-lam-tai-chinh-ke-toan-kiem-toan"), 
+                          thiet_ke_job %>% mutate(job_group = "viec-lam-thiet-ke-my-thuat"), 
+                          xay_dung_job %>% mutate(job_group = "viec-lam-xay-dung"))
 
 
-
-
-
-
-
-
-
-
-
-
-
+# Lưu lại dữ liệu: 
+write.csv(all_job_data, "D:/all_job_data.csv", row.names = TRUE)
